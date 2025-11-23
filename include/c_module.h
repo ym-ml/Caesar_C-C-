5bc5a78e17142b2b06a75b3d5c19b9087e4fc306
ymml <ym-ml@qq.com>
2025-11-23 17:03:48 +0800

    add cpp bridge in h

---
+++ include/c_module.h
@@ -0,0 +1,5 @@
+#ifndef C_MODULE_H
+#define C_MODULE_H
+
+
+#endif //C_MODULE_H
---
+++ include/cpp_moduleBridge.h
@@ -0,0 +1,16 @@
+#ifndef CPP_BRIDGE_H
+#define CPP_BRIDGE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// 在这里声明C++函数
+//void cpp_function_1(void);
+//int cpp_calculate(int a, int b);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
