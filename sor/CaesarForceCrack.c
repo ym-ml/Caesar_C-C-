81fbca7461f69594782fe803b78ed5656fbb78ca
ymml <ym-ml@qq.com>
2025-12-01 15:46:32 +0800

    forckCrack

---
+++ sor/CaesarForceCrack.c
@@ -0,0 +1,28 @@
+#include "../include/c_module.h"
+
+void CaesarForceCrack(const char* text){
+    int len=strlen(text);
+    if (len>25)len=25; //限制最大长度，防止输出过长
+
+    char result[MAX];
+    for(int key=1;key<26;key++){
+        printf("Trying key %d:\n",key);
+        for(int i=0;i<len;i++){
+            char ch=text[i];
+            if(ch>='A'&&ch<='Z'){
+                result[i]=(ch-'A'+key+26)%26+'A';
+            }
+            else if(ch>='a'&&ch<='z'){
+                result[i]=(ch-'a'+key+26)%26+'a';
+            }
+            else if(ch>='0'&&ch<='9'){
+                result[i]=(ch-'0'+key+10)%10+'0';
+            }
+            else{
+                result[i]=ch;
+            }
+        }
+        result[len]='\0';
+        printf("%s\n",result);
+    }
+}
