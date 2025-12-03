395f77ae526a1307dd27d172dc03989557709f4e
huyu-j <jpqy20060926@163.com>
2025-12-03 14:08:18 +0800

    test1

--- include/c_module.h
+++ include/c_module.h
@@ -8,14 +8,9 @@
 
 #define MAX 500
 
-<<<<<<< HEAD
-
-void get_input(char *text, int *key, int *column);
-=======
 void get_input_text(char *text);
 void get_input_keys(int *key, int *column,int choose);
 void Caesar(const char* text, int key);
 void CaesarForceCrack(const char* text);
->>>>>>> 61aca59925293e1f90b93183ef5ebc9dc2d98a24
-
+void char_count(const char* ori_char);
 #endif //C_MODULE_H
